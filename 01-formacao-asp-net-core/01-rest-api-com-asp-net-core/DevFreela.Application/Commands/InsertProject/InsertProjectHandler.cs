c77a53696f7840089f41b9fec51bedf69d530cef
Daniel Corrêa <daniel.exatas@gmail.com>
2025-01-25 08:19:56 -0300

    Adicionando notificacoes para quando um novo projeto for criado

--- 01-formacao-asp-net-core/01-rest-api-com-asp-net-core/DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
+++ 01-formacao-asp-net-core/01-rest-api-com-asp-net-core/DevFreela.Application/Commands/InsertProject/InsertProjectHandler.cs
@@ -1,18 +1,32 @@
 ﻿using DevFreela.Application.Models;
+using DevFreela.Application.Notification.ProjectCreated;
 using DevFreela.Infrastructure.Persistence;
 using MediatR;
 
 namespace DevFreela.Application.Commands.InsertProject;
 
-public class InsertProjectHandler(DevFreelaDbContext dbContext)
+public class InsertProjectHandler
     : IRequestHandler<InsertProjectCommand, ResultViewModel<int>>
 {
+    
+    private readonly DevFreelaDbContext _dbContext;
+    private readonly IMediator _mediator;
+    
+    public InsertProjectHandler(DevFreelaDbContext dbContext, IMediator mediator)
+    {
+        _dbContext = dbContext;
+        _mediator = mediator;
+    }
+    
     public async Task<ResultViewModel<int>> Handle(InsertProjectCommand request, CancellationToken cancellationToken)
     {
         var project = request.ToEntity();
 
-        dbContext.Projects.Add(project);
-        await dbContext.SaveChangesAsync();
+        _dbContext.Projects.Add(project);
+        await _dbContext.SaveChangesAsync();
+
+        var projectCreated = new ProjectCreatedNotification(project.Id, project.Title, project.TotalCost);
+        await _mediator.Publish(projectCreated);
 
         return ResultViewModel<int>.Success(project.Id);
     }
